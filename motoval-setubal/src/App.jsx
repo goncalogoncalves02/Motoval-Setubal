2e798c08d3f244827a426f1a11c229c51ed876d0
Gonçalo Filipe Brigues Gonçalves <goncalo.goncalves.t0113779@edu.atec.pt>
2026-02-22 20:11:17 +0000

    feat: Wrap application in AuthProvider and add routes for Ofertas and Admin pages

--- motoval-setubal/src/App.jsx
+++ motoval-setubal/src/App.jsx
@@ -5,20 +5,27 @@ import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import FAQPage from './pages/FAQPage';
 import SchedulePage from './pages/SchedulePage';
+import OfertasPage from './pages/OfertasPage';
+import AdminPage from './pages/AdminPage';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="contacto" element={<ContactPage />} />
-          <Route path="faq" element={<FAQPage />} />
-          <Route path="horario" element={<SchedulePage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="contacto" element={<ContactPage />} />
+            <Route path="faq" element={<FAQPage />} />
+            <Route path="horario" element={<SchedulePage />} />
+            <Route path="ofertas" element={<OfertasPage />} />
+          </Route>
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
